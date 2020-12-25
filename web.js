f2fb3421303d3630554a46100103e7997c339c44
tkyko13 <>
2020-12-25 17:09:21 +0900

    port番号を3000に

--- web.js
+++ web.js
@@ -17,11 +17,6 @@ const app = express();
 // 注文のデータを一時的に保管します
 const orderData = {};
 
-// ローカル（自分のPC）でサーバーを公開するときのポート番号です
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`server is listening... http://localhost:${process.env.PORT || 5000}`);
-});
-
 // publicフォルダのファイルを公開する
 app.use(express.static(__dirname + "/public"));
 
@@ -114,4 +109,9 @@ app.use('/pay/confirm', async (req, res) => {
         console.log('決済確認処理でエラーが発生しました。');
         console.log(e);
     };
+});
+
+// ローカル（自分のPC）でサーバーを公開するときのポート番号です
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`server is listening... http://localhost:${process.env.PORT || 3000}`);
 });
